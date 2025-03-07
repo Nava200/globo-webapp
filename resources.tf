eb871e2608884e5d81506b3a7cf0ce4627da7fc5
Nava200 <navaneetha.priyam2000@gmail.com>
2025-03-06 20:47:00 -0500

    Switch to using secrets manager

--- resources.tf
+++ resources.tf
@@ -11,20 +11,17 @@ provider "aws" {
 ##################################################################################
 
 locals {
-
   common_tags = {
     Environment = var.environment
     BillingCode = var.billing_code
   }
 
   name_prefix = "${var.prefix}-${var.environment}"
-
 }
 
 ##################################################################################
 # RESOURCES
 ##################################################################################
-
 
 resource "aws_iam_instance_profile" "main" {
   name = "${local.name_prefix}-webapp"
@@ -32,6 +29,7 @@ resource "aws_iam_instance_profile" "main" {
 
   tags = local.common_tags
 }
+
 resource "aws_instance" "main" {
   count                = length(data.tfe_outputs.networking.nonsensitive_values.public_subnets)
   ami                  = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
@@ -44,35 +42,34 @@ resource "aws_instance" "main" {
     aws_security_group.webapp_outbound_sg.id,
   ]
 
-
   key_name = module.ssh_keys.key_pair_name
 
   tags = merge(local.common_tags, {
     "Name" = "${local.name_prefix}-webapp-${count.index}"
   })
 
-  #   # Provisioner Stuff
-  #   connection {
-  #     type        = "ssh"
-  #     user        = "ec2-user"
-  #     port        = "22"
-  #     host        = self.public_ip
-  #     private_key = module.ssh_keys.private_key_openssh
-  #   }
+  # Provisioner Stuff (uncomment if needed)
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    port        = "22"
+    host        = self.public_ip
+    private_key = module.ssh_keys.private_key_openssh
+  }
 
-  #   provisioner "file" {
-  #     source      = "./templates/userdata.sh"
-  #     destination = "/home/ec2-user/userdata.sh"
-  #   }
+  provisioner "file" {
+    source      = "./templates/userdata.sh"
+    destination = "/home/ec2-user/userdata.sh"
+  }
 
-  #   provisioner "remote-exec" {
-  #     inline = [
-  #       "chmod +x /home/ec2-user/userdata.sh",
-  #       "sh /home/ec2-user/userdata.sh",
-  #     ]
-  #     on_failure = continue
-  #   }
-  user_data_replace_on_change = true
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /home/ec2-user/userdata.sh",
+      "sh /home/ec2-user/userdata.sh",
+    ]
+    on_failure = continue
+  }
+
   user_data = templatefile("./templates/userdata.sh", {
     playbook_repository = var.playbook_repository
     secret_id           = var.api_key_secret_key
@@ -80,34 +77,6 @@ resource "aws_instance" "main" {
     site_name_ssm_name  = local.site_name_ssm_name
   })
 }
-
-
-# resource "transfer_data" "webapp" {
-
-#   triggers_replace = [
-#     length(aws_instance.main.*.id),
-#     join(",", aws_instance.main.*.id)
-#   ]
-
-
-#   provisioner "file" {
-#     content = templatefile("./templates/application.config.tpl", {
-#       hosts     = aws_instance.main.*.private_dns
-#       site_name = "${local.name_prefix}-taco-wagon"
-#       api_key   = var.api_key_secret_key
-#     })
-#     destination = "/home/ec2-user/application.config"
-#   }
-
-#   connection {
-#     type        = "ssh"
-#     user        = "ec2-user"
-#     port        = "22"
-#     host        = aws_instance.main[0].public_ip
-#     private_key = module.ssh_keys.private_key_openssh
-#   }
-
-# }
 
 resource "aws_lb" "main" {
   name               = "${local.name_prefix}-webapp"
